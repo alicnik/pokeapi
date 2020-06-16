d06af92bae364f166aa39e30aaa1bf22f6d3617e
Alex Nicholas <al.nicholas@gmail.com>
2020-06-16 15:37:33 +0100

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { HashRouter, Switch, Route } from 'react-router-dom'
+
+import 'bulma'
+import './styles/style.scss'
+
+const App = () => (
+  <h1>Hello Router!</h1>
+)
+
+export default App
