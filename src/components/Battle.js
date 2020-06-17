7ab0ef3c135b794f5d7f26fadce0c7f0e76f2dc8
Michael Adair <mjadair91@gmail.com>
2020-06-17 17:35:50 +0100

    battle page

--- src/components/Battle.js
+++ src/components/Battle.js
@@ -1,10 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export const Battle = () => {
-  return (
+
+
+  return <>
+
+
     <section id="battle">
-      <h1>Battle</h1>
+      <div className="opponent">
+        <div className="opponent-stats">
+          <h3>Charizard</h3>
+          <progress value="75" max="100"></progress>
+          <p>HP: 75/100</p>
+        </div>
+        <div className="opponent-sprite">
+          <img src="../assets/charizard.png"></img>
+        </div>
+      </div>
+
+      <div className="player">
+        <div className="player-sprite">
+          <img src="../assets/Pikachu_back.png"></img>
+        </div>
+        <div className="player-stats">
+          <h3>Pikachu</h3>
+          <progress value="50" max="100"></progress>
+          <p>HP: 50/100</p>
+        </div>
+        <div className="player-options">
+          <div className="options-announce">
+            <h3> What will Pikachu do?</h3>
+          </div>
+          <div className="options-choices">
+            <ul>
+              <li>FIGHT</li>
+              <li>CHANGE POKéMON</li>
+              <li>RUN</li>
+            </ul>
+          </div>
+
+        </div>
+
+
+      </div>
+
+
+
     </section>
-  )
-  
+  </>
+
 }
